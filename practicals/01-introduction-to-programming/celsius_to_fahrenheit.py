662ee368dffc51503eb03b2ab6f622f78b08b869
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-10-01 15:00:52 +0100

    Added programs

---
+++ practicals/01-introduction-to-programming/celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    tempC = float(input('Enter the temperature in Celsius: '))
+    tempF = tempC * 1.8 + 32
+    print(f'The temperature in fahrenheit is {tempF}')
---
+++ practicals/01-introduction-to-programming/hello_name.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    name = str(input('Enter your name: '))
+    print(f'Hello {name}!')
+    
