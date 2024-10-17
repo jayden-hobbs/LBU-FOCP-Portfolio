4e87f79529c0c92eaaa2140549380cfec23009c5
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-10-17 22:50:13 +0100

    small validation

--- practicals/04-defining-functions/c_to_f_modified2.py
+++ practicals/04-defining-functions/c_to_f_modified2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 choice = str(input("""Would you like to convert from C or F? """))
-if choice == 'C':
-    temp_C = int(input("What is the temp in Degrees?"))
+if choice == 'C' or 'c':
+    temp_C = int(input("What is the temp in Degrees? "))
     print(f"The temperature in fahrenheit is {temp_C *1.8 +32}")
-elif choice == 'F':
-    temp_F = int(input("What is the temp in fahrenheit?"))
+elif choice == 'F' or 'f':
+    temp_F = int(input("What is the temp in fahrenheit? "))
     print (f"The temperature in degrees is {(temp_F - 32) / 1.8}")
 else:
     print("please enter a correct option!")
