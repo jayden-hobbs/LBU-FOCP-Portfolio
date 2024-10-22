28e9d143cdbe397703346ba9b2e070fd98a43ce4
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-10-22 14:18:35 +0100

    Slight change

--- practicals/04-defining-functions/casing_names.py
+++ practicals/04-defining-functions/casing_names.py
@@ -2,11 +2,10 @@
 
 def get_name():
     input_name = str(input("Enter your name: "))
-    lower_name = input_name.lower
-    print(f'{lower_name().capitalize()}')
+    print(f'{input_name.capitalize()}')
+
+
+
+
 
 get_name()
-
-
-
-
