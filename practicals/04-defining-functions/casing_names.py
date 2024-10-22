58d5ac68de86f1f2752925d2590e101ecd525eca
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-10-22 14:09:48 +0100

    Added casing_names

---
+++ practicals/04-defining-functions/casing_names.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def get_name():
+    input_name = str(input("Enter your name: "))
+    lower_name = input_name.lower
+    print(f'{lower_name().capitalize()}')
+
+get_name()
+
+
+
+
