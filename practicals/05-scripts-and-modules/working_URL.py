48dc59d6d8c313b25482e63883d3fa9758d1b832
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-05 17:49:09 +0000

    Added a try and except command

--- practicals/05-scripts-and-modules/working_URL.py
+++ practicals/05-scripts-and-modules/working_URL.py
@@ -3,6 +3,15 @@
 import requests
 
 if __name__ == '__main__':
-    url = input('Enter a URL: ')
-    response = requests.get(url)
-    print(response.status_code)
+
+    try:
+        url = input('Enter a URL: ')
+        response = requests.get(url)
+        if response.status_code == 200:
+            print('Success!')
+        elif response.status_code == 404:
+            print('Not Found.')
+        else:
+            print(f'Something went wrong. The response is {response.status_code}')
+    except requests.exceptions.MissingSchema or requests.exceptions.InvalidSchema or requests.exceptions.InvalidURL:
+        print('Invalid URL')
