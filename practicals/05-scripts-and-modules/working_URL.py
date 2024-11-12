71927bd27939a9615c32cdf293b709f94d4f5cf6
Tony Jenkins <A.M.Jenkins@leedsbeckett.ac.uk>
2024-11-12 14:15:39 +0000

    Update URL program

--- practicals/05-scripts-and-modules/working_URL.py
+++ practicals/05-scripts-and-modules/working_URL.py
@@ -5,13 +5,15 @@ import requests
 if __name__ == '__main__':
     url = str(input("Enter a URL: "))
     status_codes = {}
-    with open('C:/Users/Seagu/OneDrive - Leeds Beckett University/Computer Programming/Visual Studio Code/programming-portfolio-jayden-hobbs/practicals/05-scripts-and-modules/status_codes.txt', 'r') as file:
-        for line in file:
-            parts = line.split().split(' ', 1)
-            if len == 2:
-                code, message = parts
-                status_codes[int(code)] = message
+    with open('status_codes.txt', 'r') as in_file:
+        for line in in_file:
+            code, message = line.split(' ', 1)
+            status_codes[int(code)] = message.strip()
 
-    response = requests.get(url)
-    status_message = status_codes.get(response.status_code, f'Unknown status code: {response.status_code}')
-    print(f'The status code is: {response.status_code} - {status_message}')
+    try:
+        response = requests.get(url)
+    except Exception as e:
+        print('Error connecting to web host.')
+    finally:
+        status_message = status_codes.get(response.status_code, f'Unknown status code: {response.status_code}')
+        print(f'The status code is: {response.status_code} - {status_message}')
