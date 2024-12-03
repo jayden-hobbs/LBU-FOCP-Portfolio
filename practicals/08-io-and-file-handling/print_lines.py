834c9864f3918b987c73db9de3642738e803e8ca
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-03 15:05:09 +0000

    Added first 08 programs

---
+++ practicals/08-io-and-file-handling/print_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    filename = 'program1.txt'
+    with open(filename, 'r') as file:
+        for line_number, line in enumerate(file, 1):
+            print(f"{line_number}\t{line.strip()}")
+
