cae18a9953d79ae411220382470a30728de40192
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-12 12:41:08 +0000

    modified code

--- practicals/99-additional-work/binaryIP.py
+++ practicals/99-additional-work/binaryIP.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
-#! This code takes in an IP address as an input and outputs in binary and hexadecimal form
+# This code takes an IP address as input and outputs its binary and hexadecimal form
 
 IP = input("Enter an IP address: ")
-binIP = bin(int(IP))[2:]
-hexIP = hex(int(IP))[2:]
 
-print(f"Binary: {binIP} \nHexadecimal: {hexIP}")
+octets = IP.split('.')
+
+binIP = ''
+hexIP = ''
+
+for octet in octets:
+    num = int(octet)
+
+    binIP += bin(num)[2:].zfill(8)  
+    hexIP += hex(num)[2:].zfill(2)  
+
+print(f"Binary: {binIP}")
+print(f"Hexadecimal: {hexIP}")
