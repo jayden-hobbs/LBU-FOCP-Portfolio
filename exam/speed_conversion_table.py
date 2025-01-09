0fca6595895560e5dba548bda059876b870f1edc
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2025-01-09 15:38:00 +0000

    modification to commit #3

--- exam/speed_conversion_table.py
+++ exam/speed_conversion_table.py
@@ -2,17 +2,11 @@
 
 from tabulate import tabulate
 
-def conversion(mph):
-    return mph * 1.60934
-
-def main():
+if __name__ == '__main__':
     speed = []
     for mph in range(0, 75, 5):
-        kmph = conversion(mph)
+        kmph = mph * 1.60934
         speed.append([mph, round(kmph, 2)])
     
     table = tabulate(speed, headers=["Miles per Hour", "Kilometres per Hour"], tablefmt="fancy_grid")
     print(table)
-
-if __name__ == '__main__':
-    main()
