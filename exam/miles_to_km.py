71f8659438ecbdffcb618db82ac499a027f60e06
Jayden <jaydenhobbs1@icloud.com>
2025-01-19 23:14:20 +0000

    bug fix

--- exam/miles_to_km.py
+++ exam/miles_to_km.py
@@ -4,17 +4,12 @@ if __name__ == '__main__':
     while True:
         try:
             miles = int(input("Please enter the distance in miles: "))
-            if miles < 0:
+            if miles >= 0: 
                 km = miles * 1.60934
-                return miles
-                return km
-                break
+                km_rounded = round(km, 2)
+                print(f'{miles} miles is {km_rounded} kilometres')
+                break  
+            else:
+                print("Please enter a value greater than or equal to 0.")  
         except ValueError:
-            print("Please enter an integer greater than or equal to 0")
-
-    print(f'{miles} miles is {km} kilometres')
-
-
-       
-
-
+            print("Please enter a valid integer greater than or equal to 0.")  
